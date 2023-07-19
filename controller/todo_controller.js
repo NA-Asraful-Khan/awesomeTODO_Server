2703350700286d19ffc86e166f317ffd81bc81d3
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-07-19 13:05:18 +0600

    Get route done

--- controller/todo_controller.js
+++ controller/todo_controller.js
@@ -12,3 +12,11 @@ export const addTodo = async(req,res)=>{
        return res.status(500).json(error.message); 
     }
 }
+export const getAllToDo = async(req,res)=>{
+    try {
+        const todos = await todo.find({}).sort({'createdAt': -1})
+        return res.status(200).json(todos)
+    } catch (error) {
+       return res.status(500).json(error.message); 
+    }
+}
--- routes/route.js
+++ routes/route.js
@@ -1,9 +1,10 @@
 import express from 'express';
-import { addTodo } from '../controller/todo_controller.js';
+import { addTodo, getAllToDo } from '../controller/todo_controller.js';
 
 const route = express.Router();
 
 
+route.get('/todos',getAllToDo)
 route.post('/todos',addTodo)
 
 export default route;
