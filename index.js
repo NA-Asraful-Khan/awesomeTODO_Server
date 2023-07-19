fa0cef9e5cf0cfccb59c350e57df70bf0dac9544
NA-Asraful-Khan <soyonshikhor@gmail.com>
2023-07-19 11:01:23 +0600

    connected with frontend

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
 import express from 'express'
-
+import cors from 'cors'
 import Connection from './database/db.js';
 
 const app = express ();
+app.use(cors());
 const PORT= 8000;
 
 Connection()
 
 app.listen(PORT,()=>{
     console.log(`Server is running at http://localhost:${PORT}/`)
+})
+
+app.get('/',(req,res)=>{
+    res.send('Welcome To the Server');
 })
